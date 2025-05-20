068c1b8444ef5b8cbc9a50a27bd559e6d9080cde
Ajith7736 <ajith.aju39502@gmail.com>
2025-05-20 19:11:03 +0530

    slash

--- javascript/script.js
+++ javascript/script.js
@@ -37,14 +37,14 @@ async function getsongs(folder) {
     songlist.innerHTML = ""
     for (const song of songs) {
         songlist.innerHTML += `<li>
-        <img class="invert" src="/img/music.svg" alt="">
+        <img class="invert" src="img/music.svg" alt="">
                         <div class="info">
                             <div>${song.replaceAll("%20", " ")}</div>
                             <div></div>
                         </div>
                         <div class="playnow">
                             <span>Play Now</span>
-                            <img class="invert play-btn" src="/img/play.svg" alt="">
+                            <img class="invert play-btn" src="img/play.svg" alt="">
                         </div>
                     
         </li>`
@@ -59,10 +59,10 @@ async function getsongs(folder) {
     return songs;
 }
 const playmusic = (track, pause = false) => {
-    currentSong.src = `/songs/${currfolder}/` + track
+    currentSong.src = `songs/${currfolder}/` + track
     if (!pause) {
         currentSong.play()
-        play.src = "/img/pause.svg"
+        play.src = "img/pause.svg"
     }
     document.querySelector(".songinfo").innerHTML = decodeURI(track)
     document.querySelector(".songtime").innerHTML = "00:00/00:00"
@@ -93,7 +93,7 @@ async function displayalbums() {
                                 d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z" />
                         </g>
                     </svg>
-                    <img src="/songs/${folder}/cover.jpg" alt="">
+                    <img src="songs/${folder}/cover.jpg" alt="">
                     <h3>${response.title}</h3>
                     <p class="grey">${response.description}</p>
                 </div>`
@@ -127,11 +127,11 @@ async function main() {
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play()
-            play.src = "/img/pause.svg"
+            play.src = "img/pause.svg"
         }
         else {
             currentSong.pause()
-            play.src = "/img/play.svg"
+            play.src = "img/play.svg"
         }
     })
     // time update
@@ -139,9 +139,9 @@ async function main() {
         document.querySelector(".songtime").innerHTML = formatTimeDisplay(currentSong.currentTime, currentSong.duration)
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%"
         if (document.querySelector(".circle").style.left == "100%") {
-            play.src = "/img/play.svg"
+            play.src = "img/play.svg"
         } else if (!currentSong.paused) {
-            play.src = "/img/pause.svg"
+            play.src = "img/pause.svg"
         }
     })
     // Add even listener to seekbar
@@ -205,9 +205,9 @@ async function main() {
     document.querySelector(".range-input").addEventListener("change", (e) => {
         currentSong.volume = parseInt(e.target.value) / 100
         if (currentSong.volume == 0) {
-            document.querySelector(".volume-svg").src = "/img/volumeoff.svg"
+            document.querySelector(".volume-svg").src = "img/volumeoff.svg"
         } else {
-            document.querySelector(".volume-svg").src = "/img/volume.svg"
+            document.querySelector(".volume-svg").src = "img/volume.svg"
         }
     })
     // volume button event listener
@@ -215,11 +215,11 @@ async function main() {
         if (currentSong.volume > 0) {
             currentSong.volume = 0
             document.querySelector(".range-input").value = 0
-            document.querySelector(".volume-svg").src = "/img/volumeoff.svg"
+            document.querySelector(".volume-svg").src = "img/volumeoff.svg"
         } else {
             currentSong.volume = 0.5
             document.querySelector(".range-input").value = 50
-            document.querySelector(".volume-svg").src = "/img/volume.svg"
+            document.querySelector(".volume-svg").src = "img/volume.svg"
         }
     })
 }
